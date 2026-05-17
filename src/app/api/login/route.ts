baf280cd7cca80d9c0391a4edbe5e625ce28274b
Hiyori <nozayiyc@gmail.com>
2026-05-17 15:02:01 +0200

    Fix 5

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -4,19 +4,62 @@ import bcrypt from "bcryptjs";
 
 import { createToken } from "../../../../lib/auth";
 
+const attempts = new Map<
+  string,
+  {
+    count: number;
+    blockedUntil?: number;
+  }
+>();
+
 export async function POST(req: Request) {
   try {
     const body = await req.json();
 
     const { username, password } = body;
 
-    /* IDENTIFIANT */
-    if (username !== process.env.ADMIN_USERNAME) {
+    const ip = req.headers.get("x-forwarded-for") || "unknown";
+
+    const attempt = attempts.get(ip);
+
+    /* BLOCKED */
+
+    if (attempt?.blockedUntil && Date.now() < attempt.blockedUntil) {
       return NextResponse.json(
         {
           success: false,
+
+          error: "Trop de tentatives. Réessaie plus tard.",
+        },
+
+        {
+          status: 429,
+        },
+      );
+    }
+
+    /* IDENTIFIANT */
+
+    if (username !== process.env.ADMIN_USERNAME) {
+      const current = attempts.get(ip) || {
+        count: 0,
+      };
+
+      current.count++;
+
+      if (current.count >= 3) {
+        current.blockedUntil = Date.now() + 1000 * 60 * 5;
+      }
+
+      attempts.set(ip, current);
+
+      return NextResponse.json(
+        {
+          success: false,
+
           error: "Identifiants invalides",
         },
+
         {
           status: 401,
         },
@@ -24,24 +67,45 @@ export async function POST(req: Request) {
     }
 
     /* PASSWORD */
+
     const validPassword = await bcrypt.compare(
       password,
+
       process.env.ADMIN_PASSWORD_HASH!,
     );
 
     if (!validPassword) {
+      const current = attempts.get(ip) || {
+        count: 0,
+      };
+
+      current.count++;
+
+      if (current.count >= 3) {
+        current.blockedUntil = Date.now() + 1000 * 60 * 5;
+      }
+
+      attempts.set(ip, current);
+
       return NextResponse.json(
         {
           success: false,
+
           error: "Identifiants invalides",
         },
+
         {
           status: 401,
         },
       );
     }
 
+    /* RESET ATTEMPTS */
+
+    attempts.delete(ip);
+
     /* TOKEN */
+
     const token = createToken();
 
     const response = NextResponse.json({
@@ -69,8 +133,10 @@ export async function POST(req: Request) {
     return NextResponse.json(
       {
         success: false,
+
         error: "Erreur serveur",
       },
+
       {
         status: 500,
       },
